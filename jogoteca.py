b9525452bf0e8588dcd6c1fe75f5303e1a58e7e3
fraanpsilva <francilenesilva.pt@gmail.com>
2022-11-06 18:39:25 +0000

    criando um novo jogo

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,18 +6,38 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista_de_jogos = [jogo1, jogo2, jogo3]
+
+
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+@app.route('/')
+def index():
 
-    lista_de_jogos = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogo', jogos = lista_de_jogos)
 
+@app.route('/novo-jogo')
+def novoJogo():
+    return render_template('novo-jogo.html', titulo= 'Novo Jogo')
 
-app.run()
+@app.route('/criar', methods=['POST',])
+def criaJogo():
+    # capturando as informações do formulario
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    # criando um objeto com as informações do formulário
+    jogo = Jogo(nome, categoria, console)
+
+    # adicionando na lista de jogos
+    lista_de_jogos.append(jogo)
+
+    return render_template('lista.html', titulo= 'Jogos', jogos=lista_de_jogos)
+
+app.run(debug=True)
 # trecho da app caso quiser alterar a porta
 # app.run(host='0.0.0.0', port=8080)
